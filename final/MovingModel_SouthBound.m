f189f9a185bb3d9687091ed2e409ffb02238fbdb
yccjoseph <joseph3yang@gmail.com>
2017-06-19 00:19:18 +0800

    update MovingModels and add models for South bound trains

--- final/MovingModel_SouthBound.m
+++ final/MovingModel_SouthBound.m
@@ -1,9 +1,9 @@
-function ME = MovingModel(ME, maxSpeed, acc, dec, MRT_s)
+function ME = MovingModel_SouthBound(ME, maxSpeed, acc, dec, MRT_s)
     if ME.status == -2
         ME.pauseTime = ME.pauseTime - 1;
         if ME.pauseTime == 0
-            ME.id_station_start = ME.id_station_start + 1;
-            ME.id_station_end = ME.id_station_end + 1;
+            ME.id_station_start = ME.id_station_start - 1;
+            ME.id_station_end = ME.id_station_end - 1;
             distToNextStation = sqrt((MRT_s{1, ME.id_station_end}.pos_x - MRT_s{1, ME.id_station_start}.pos_x)^2 + ...
                             (MRT_s{1, ME.id_station_end}.pos_y - MRT_s{1, ME.id_station_start}.pos_y)^2);
             ME.unitvector_x = (MRT_s{1, ME.id_station_end}.pos_x - MRT_s{1, ME.id_station_start}.pos_x)/distToNextStation;
@@ -14,21 +14,25 @@ function ME = MovingModel(ME, maxSpeed, acc, dec, MRT_s)
             if ME.id_station_end == 10
                 ME.pauseTime = 40;
             end
+            ME.reconfig = 1;
         end
         return
+    else
+        ME.reconfig = 0;
     end
-    distToDestination = sqrt((MRT_s{1, ME.id_station_end}.pos_x - ME.pos_x)^2 + (MRT_s{1, ME.id_station_end}.pos_y - ME.pos_y)^2);
+    distToDestination = sqrt((MRT_s{1, ME.id_station_end}.pos_x + ME.pos_relativeToStation_x - ME.pos_x)^2 ...
+                           + (MRT_s{1, ME.id_station_end}.pos_y + ME.pos_relativeToStation_y - ME.pos_y)^2);
     if distToDestination <= ME.velocity
-        ME.pos_x = MRT_s{1, ME.id_station_end}.pos_x;
-        ME.pos_y = MRT_s{1, ME.id_station_end}.pos_y;
+        ME.pos_x = MRT_s{1, ME.id_station_end}.pos_x + ME.pos_relativeToStation_x;
+        ME.pos_y = MRT_s{1, ME.id_station_end}.pos_y + ME.pos_relativeToStation_y;
         ME.status = -2;
         ME.velocity = 0;
-        scatter(ME.pos_x, ME.pos_y, 'g');
+        % scatter(ME.pos_x, ME.pos_y, 'g');
         return
     end
     ME.pos_x = ME.pos_x + ME.velocity*ME.unitvector_x;
     ME.pos_y = ME.pos_y + ME.velocity*ME.unitvector_y;
-    scatter(ME.pos_x, ME.pos_y, 'g');
+    % scatter(ME.pos_x, ME.pos_y, 'g');
     if (ME.status == 1) && ME.velocity < maxSpeed
         ME.velocity = ME.velocity + acc;
     end
