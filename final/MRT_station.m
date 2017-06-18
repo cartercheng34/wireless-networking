61a6f3265da0a8e1adae150fce262963032bddfa
yccjoseph <joseph3yang@gmail.com>
2017-06-19 00:17:20 +0800

    add mean value of people getting on and off the train

--- final/MRT_station.m
+++ final/MRT_station.m
@@ -3,12 +3,16 @@ classdef MRT_station
         pos_x;
         pos_y;
         id
+        lambda_in
+        lambda_out
     end
     methods
-        function M = MRT_station(x , y , id)
+        function M = MRT_station(x , y , id, in, out)
             M.pos_x = x;
             M.pos_y = y;
             M.id = id;
+            M.lambda_in = in;
+            M.lambda_out = out;
         end
     end
 end
